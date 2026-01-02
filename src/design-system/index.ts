89a9980767d0933a90fa842b9ee6af1ea908fb8e
Disaster Recovery Dev Team <dev@disasterrecovery.com.au>
2026-01-02 13:29:54 +1000

    Update DesignOS component exports

--- src/design-system/index.ts
+++ src/design-system/index.ts
@@ -19,3 +19,6 @@ export { PriorityCard, priorityCardVariants, type PriorityCardProps } from './co
 export { EmergencyCTA, StickyEmergencyCTA, type EmergencyCTAProps } from './components/EmergencyCTA/EmergencyCTA';
 export { FormInput, type FormInputProps } from './components/Form/FormInput';
 export { LoadingProgress, LoadingOverlay, type LoadingProgressProps } from './components/LoadingProgress/LoadingProgress';
+export { Header, type HeaderProps } from './components/Navigation/Header';
+export { ErrorState, type ErrorStateProps, type AlternativeAction } from './components/ErrorState/ErrorState';
+export { IICRCBadge, IICRCBadgeGroup, type IICRCBadgeProps, type IICRCCode } from './components/Badge/IICRCBadge';
